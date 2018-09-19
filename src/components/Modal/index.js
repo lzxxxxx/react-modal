b8efdc8405142e3da13437f8f9faed639585ffdc
liuzixu <liuzixu@xiaomi.com>
2018-09-19 15:03:11 +0800

    start

---
+++ src/components/Modal/index.js
@@ -0,0 +1,14 @@
+import {Component} from 'react';
+
+class Modal extends Component {
+  constructor (props){
+    super(props)
+  }
+  render (){
+    return (
+      
+    )
+  }
+}
+
+export default Modal;
